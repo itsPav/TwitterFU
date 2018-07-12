eb098c838034b5c280c3a590fe7c99ddfae469b5
Pav <pavel-m@live.com>
2018-07-12 07:14:09 -0400

    Added config file

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var T = require('./js/config');
+const T = require('./js/config');
+const fs = require('fs');
+const json2csv = require('json2csv').Parser;
+
+const fields = ['date', 'accountName', 'follows', 'unfollows', 'totalFollowers', 'totalFollowing'];
 
 var accountData;
 
-// Get the user data of the api
+var myData = {};
+
+// Get the user data
 T.get('account/verify_credentials', { skip_status: true })
   .catch(function (err) {
     // console.log('caught error', err.stack)
@@ -12,42 +18,69 @@ T.get('account/verify_credentials', { skip_status: true })
     accountData = result.data;
     console.log(accountData.screen_name);
 
-    // GET friends/ids
-    T.get('friends/ids', { screen_name: `${accountData.screen_name}` },  function (err, data, response) {
-        // console.log('Followers');
-        // console.log(data.ids)
+    // // GET friends/ids
+    // T.get('friends/ids', { screen_name: `${accountData.screen_name}` },  function (err, data, response) {
+    //     // console.log('Followers');
+    //     // console.log(data.ids)
 
-        var toUnfollow = [];
-        (data.ids).forEach(element => {
-          if(toUnfollow.length < 10 && element != "") {
-            toUnfollow.push(element);
-          }
+    //     var toUnfollow = [];
+    //     (data.ids).forEach(element => {
+    //       if(toUnfollow.length < 10 && element != "") {
+    //         toUnfollow.push(element);
+    //       }
+    //     });
+
+    //     // Unfollow
+    //     // https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-destroy
+    //     toUnfollow.forEach(element => {
+    //       T.post('friendships/destroy', { user_id: element }, function (err, data, response) {
+    //           console.log('Unfollowed: ' + element)
+    //       })
+    //     })
+    // })
+
+    // var followCount = 0;
+
+    // var stream = T.stream('statuses/sample')
+    // // get real time sample tweet ids
+    // stream.on('tweet', function (tweet) {
+    // // console.log(tweet.user.id)
+    // // Follow
+    // // https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-create.html
+    //   T.post('friendships/create', { user_id: tweet.user.id }, function (err, data, response) {
+    //     console.log('Followed: ' + tweet.user.id);
+    //     followCount++;
+    //   });
+    // });
+
+
+    myData.date = Date();
+    myData.accountName = 'Hello';
+    myData.follows = 5;
+    myData.unfollows = 2;
+
+    try {
+
+      const parser = new json2csv({ fields });
+      const csv = parser.parse(myData);
+
+      console.log(csv);
+
+      if(fs.existsSync('followerStats.csv')){
+        console.log('Exists');
+        fs.appendFile('followerStats.csv', `"${myData.date}","${myData.accountName}",${myData.follows},${myData.unfollows},"",""` + "\n", (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
         });
-
-        // Unfollow
-        // https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-destroy
-        toUnfollow.forEach(element => {
-          T.post('friendships/destroy', { user_id: element }, function (err, data, response) {
-              console.log('Unfollowed: ' + element)
-          })
-        })
-    })
-
-    var followCount = 0;
-
-    setTimeout(function(){
-      var stream = T.stream('statuses/sample')
-      // get real time sample tweet ids
-      while(followCount <= 20) {
-        stream.on('tweet', function (tweet) {
-        // console.log(tweet.user.id)
-        // Follow
-        // https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-create.html
-          T.post('friendships/create', { user_id: tweet.user.id }, function (err, data, response) {
-            console.log('Followed: ' + tweet.user.id);
-            followCount++;
-          });
+      } else {
+        console.log('Does not exist');
+        fs.writeFile('followerStats.csv', csv + "\n", (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
         });
       }
-    }, 2000);
+    } catch (err) {
+      console.error(err);
+    }
+
 });
