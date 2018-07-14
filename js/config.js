813074094856bb63f48b6d1024ba81d9588fb8b6
Pav <pavel-m@live.com>
2018-07-13 23:47:33 -0400

    Removing config file.

--- js/config.js
+++ js/config.js
@@ -3,19 +3,19 @@ var Twit = require('twit')
 var accounts = [];
 
 var T1 = new Twit({
-  consumer_key:         '2SDXEdM92vSCZznM91JncFbN4',
-  consumer_secret:      'c82L26u0Nnjm0SJQW6HEyC4kuOCFvA95DOR6bQ5XHx0K5uDacC',
-  access_token:         '883308196689051648-Nd5kAkdSgZddJHlPmaD4t8UBKwjVYwR',
-  access_token_secret:  'hC8MaDg77cWkxsb2oYPWrEbjjrIcQpoXgLbWchB6ORiOb',
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
 
 // var T2 = new Twit({
-//   consumer_key:         '2SDXEdM92vSCZznM91JncFbN4',
-//   consumer_secret:      'c82L26u0Nnjm0SJQW6HEyC4kuOCFvA95DOR6bQ5XHx0K5uDacC',
-//   access_token:         '883308196689051648-Nd5kAkdSgZddJHlPmaD4t8UBKwjVYwR',
-//   access_token_secret:  'hC8MaDg77cWkxsb2oYPWrEbjjrIcQpoXgLbWchB6ORiOb',
+//   consumer_key:         '...',
+//   consumer_secret:      '...',
+//   access_token:         '...',
+//   access_token_secret:  '...',
 //   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 //   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 // })
