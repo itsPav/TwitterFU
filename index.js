6e3a1750bd74027abeec6de6ddbe1052d63a5808
Pav <pavel-m@live.com>
2018-07-13 22:37:55 -0400

    Added heroku port.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const accounts = require('./js/config');
 const csv = require('csvtojson')
 const json2csv = require('json2csv').Parser;
 const fields = ['date', 'accountName', 'totalFollowers', 'totalFollowing'];
+const PORT = process.env.PORT || 5000
 
 var minutes = 120;
 var the_interval = minutes * 60 * 1000;
@@ -41,8 +42,8 @@ io.on('connection', function(socket){
     })
   });  
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
       
 
